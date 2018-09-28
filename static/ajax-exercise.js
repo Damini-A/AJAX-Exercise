053fa999a2ea214f98043a640a9ee9134ea9e8e7
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-28 12:55:41 -0700

    AJAX Complete Fortune & Weather

---
+++ static/ajax-exercise.js
@@ -0,0 +1,55 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+function fortuneSuccess(fortune) {
+  //console.log("fortune: " + fortune);
+  $('#fortune-text').html(fortune);
+}
+
+function showFortune(evt) {
+
+    // TODO: get the fortune and show it in the #fortune-text div
+   // evt.preventDefault();
+    //console.log("Requestion from server.");
+    $.get("/fortune", fortuneSuccess);
+}
+
+$('#get-fortune-button').on('click', showFortune);
+
+
+
+
+
+// PART 2: SHOW WEATHER
+function weatherSuccess(results) {
+  $('#weather-info').html(results['forecast']); 
+}
+
+function showWeather(evt) {
+    evt.preventDefault();
+
+    let url = "/weather.json";
+    let formData = {"zipcode": $("#zipcode-field").val()};
+    $.get(url, formData, weatherSuccess);
+
+    // TODO: request weather with that URL and show the forecast in #weather-info
+}
+
+$("#weather-form").on('submit', showWeather);
+
+
+
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+    evt.preventDefault();
+
+    // TODO: show the result message after your form
+    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+}
+
+$("#order-form").on('submit', orderMelons);
+
+
